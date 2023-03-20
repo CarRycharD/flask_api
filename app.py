848012e100f400a558eaa3026169002da0eb3a5c
Richárd Vaszi <64356862+CarRycharD@users.noreply.github.com>
2023-03-20 23:25:30 +0100

    feat(app.py): added admin claim

--- app.py
+++ app.py
@@ -33,6 +33,13 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "253362580375395410082366656293858963850"
     jwt = JWTManager(app)
 
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        # Look in the db and see whether the user is an admin
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
